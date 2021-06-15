96b1881bdb5e518fb24dac2c59e2139412a44386
me-cafebabe <me_cafebabe@hotmail.com>
2021-06-15 09:06:01 +0800

    mfd: ti-lmu: Import Xiaomi changes
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- drivers/mfd/ti-lmu.c
+++ drivers/mfd/ti-lmu.c
@@ -34,6 +34,10 @@ static int ti_lmu_enable_hw(struct ti_lmu *lmu, enum ti_lmu_id id)
 {
 	int ret;
 
+#ifdef CONFIG_MACH_XIAOMI_SDM439
+	pr_err("[bkl] %s enter\n", __func__);
+#endif
+
 	if (gpio_is_valid(lmu->en_gpio)) {
 		ret = devm_gpio_request_one(lmu->dev, lmu->en_gpio,
 					    GPIOF_OUT_INIT_HIGH, "lmu_hwen");
@@ -53,6 +57,10 @@ static int ti_lmu_enable_hw(struct ti_lmu *lmu, enum ti_lmu_id id)
 					  LM3631_LCD_EN_MASK,
 					  LM3631_LCD_EN_MASK);
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_SDM439
+	pr_err("[bkl] %s finish\n", __func__);
+#endif
 
 	return 0;
 }
@@ -177,6 +185,18 @@ static int ti_lmu_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	struct ti_lmu *lmu;
 	int ret;
 
+#ifdef CONFIG_MACH_XIAOMI_SDM439
+	extern char *saved_command_line;
+	int bkl_id = 0;
+	char *bkl_ptr = (char *)strnstr(saved_command_line, ":bklic=", strlen(saved_command_line));
+	bkl_ptr += strlen(":bklic=");
+	bkl_id = simple_strtol(bkl_ptr, NULL, 10);
+	if (bkl_id != 1) {
+		return -ENODEV;
+	}
+	pr_err("[bkl] %s enter\n", __func__);
+#endif
+
 	match = of_match_device(ti_lmu_of_match, dev);
 	if (!match)
 		return -ENODEV;
@@ -217,6 +237,10 @@ static int ti_lmu_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	BLOCKING_INIT_NOTIFIER_HEAD(&lmu->notifier);
 
 	i2c_set_clientdata(cl, lmu);
+
+#ifdef CONFIG_MACH_XIAOMI_SDM439
+	pr_err("[bkl] %s finish\n", __func__);
+#endif
 
 	return mfd_add_devices(lmu->dev, 0, data->cells,
 			       data->num_cells, NULL, 0, NULL);
