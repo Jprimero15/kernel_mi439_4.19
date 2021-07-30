67389cc652c3ab3676e2428550598d7b5fa65cd8
Lucas Wei <lucaswei@google.com>
2021-07-30 15:40:20 +0800

    Revert "usb: dwc3: debugfs: Add and remove endpoint dirs dynamically"
    This reverts commit 3bef21035888dd19750ad79b5d08fe52a02ad719.
    
    Reason for revert: Boot fail with this patch.
    Bug: 194984062
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: Ice1c308f6ff0255a5f541a6351ceebcda6b3ddca

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -819,7 +819,8 @@ static void dwc3_debugfs_create_endpoint_files(struct dwc3_ep *dep,
 	}
 }
 
-void dwc3_debugfs_create_endpoint_dir(struct dwc3_ep *dep)
+static void dwc3_debugfs_create_endpoint_dir(struct dwc3_ep *dep,
+		struct dentry *parent)
 {
 	struct dentry		*dir;
 
@@ -830,6 +831,21 @@ void dwc3_debugfs_create_endpoint_dir(struct dwc3_ep *dep)
 	}
 
 	dwc3_debugfs_create_endpoint_files(dep, dir);
+}
+
+static void dwc3_debugfs_create_endpoint_dirs(struct dwc3 *dwc,
+		struct dentry *parent)
+{
+	int			i;
+
+	for (i = 0; i < dwc->num_eps; i++) {
+		struct dwc3_ep	*dep = dwc->eps[i];
+
+		if (!dep)
+			continue;
+
+		dwc3_debugfs_create_endpoint_dir(dep, parent);
+	}
 }
 
 static ssize_t dwc3_store_int_events(struct file *file,
@@ -1040,6 +1056,7 @@ void dwc3_debugfs_init(struct dwc3 *dwc)
 				    &dwc3_testmode_fops);
 		debugfs_create_file("link_state", S_IRUGO | S_IWUSR, root, dwc,
 				    &dwc3_link_state_fops);
+		dwc3_debugfs_create_endpoint_dirs(dwc, root);
 
 		file = debugfs_create_file("int_events", 0644, root, dwc,
 				&dwc3_gadget_dbg_events_fops);
